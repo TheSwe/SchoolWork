4185022b27b6d929b7070d1a63f0ab9350616607
TheSwe <60827574+TheSwe@users.noreply.github.com>
2024-05-20 10:37:53 +0200

    Changes to login and register pages

--- Html/Store/js-folder/save-login.js
+++ Html/Store/js-folder/save-login.js
@@ -1,12 +1,44 @@
 function login(form) {
     let username = form.username.value;
-    sessionStorage.setItem("username", username);
-    document.getElementById("navbarname").textContent = username;
+    let password = form.password.value;
+    const users = sessionStorage.getItem("users").split(",");
+    const passwords = sessionStorage.getItem("passwords").split(",");
+
+    if (users.includes(username)) {
+        let index = users.indexOf(username);
+        if (password === myValues[index]){
+            sessionStorage.setItem("username", username);
+            document.getElementById("navbarname").textContent = username;
+            document.getElementById("navbarname2").textContent = username;
+        }
+    }
 }
 function navbarname(){
     let name = sessionStorage.getItem("username");
     if (name != null){
-        document.getElementById("navbarname").textContent = name
-        document.getElementById("navbarname2").textContent = name
+        document.getElementById("navbarname").textContent = name;
+        document.getElementById("navbarname2").textContent = name;
     }
+}
+function register(form){
+    if (!sessionStorage.getItem("users")){
+        const users = [form.username.value];
+        sessionStorage.setItem("users", users);
+
+        const passwords = [form.password.value];
+        sessionStorage.setItem("passwords", passwords);
+    }
+    else if (!sessionStorage.getItem("users").includes(form.username.value+",")) {
+        const users = sessionStorage.getItem("users").split(",");
+        users.push(form.username.value);
+        sessionStorage.setItem("users", users);
+
+        const passwords = sessionStorage.getItem("passwords").split(",");
+        passwords.push(form.password.value);
+        sessionStorage.setItem("passwords", passwords);
+    }
+    else {
+        alert("Username already taken");
+    }
+    
 }
