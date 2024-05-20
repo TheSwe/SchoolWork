dc5d1a5e1471ae4ae84a5656cf20d9f4bc16b3ed
TheSwe <60827574+TheSwe@users.noreply.github.com>
2024-05-20 11:00:00 +0200

    Minor work on flöde i kanal

--- LallKod/flödeIKanal.py
+++ LallKod/flödeIKanal.py
@@ -1,4 +1,5 @@
-stepSize = 0.1
+stepSize = 0.01
+
 flowConstant = 0.5
 flowLinearIncrease = 0.66666666667
 
@@ -10,7 +11,7 @@ centerValue = 3
 currentValue = 0
 flowRateTotal = 0
 
-while currentValue <= centerValue:
+while currentValue < centerValue:
     flowRateTotal += (shapeConstant + shapeLinearIncrease*currentValue)*stepSize*(flowConstant + flowLinearIncrease*currentValue)
     print (flowRateTotal)
     currentValue += stepSize
